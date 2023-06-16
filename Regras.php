b28b13c0be1f65f2ff5d5725dba8ee81f3e85436
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-16 09:24:15 -0300

    Update Regras.php

--- Regras.php
+++ Regras.php
@@ -1,2 +1,7 @@
 // Regras para nomes identificadores no PHP
 
+/*
+1- Variáveis sempre começam com o símbolo $;
+2- O segundo caractere após o $ poder ser letra ou o símbolo: _;
+Aceita caracteres [a-z], [A-Z], [0-9] e [_]
+*/
