64c4a7df160740e21c78cbcae09665d4cbac91f0
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-16 09:29:53 -0300

    Update Regras.php

--- Regras.php
+++ Regras.php
@@ -3,5 +3,9 @@
 /*
 1- Variáveis sempre começam com o símbolo $;
 2- O segundo caractere após o $ poder ser letra ou o símbolo: _;
-Aceita caracteres [a-z], [A-Z], [0-9] e [_]
+3- Aceita caracteres [a-z], [A-Z], [0-9] e [_];
+4- Aceita caracteres da tabela ASCII a partir de 128, (caracteres acentuados nas Variáveis);
+5- Aceita caracteres acentuados como: á, õ, ç;
+6- A linguagem é case sensite em relação aos nomes, colocar "";
+7- Nomes especiais como $this não podem ser usados.
 */
