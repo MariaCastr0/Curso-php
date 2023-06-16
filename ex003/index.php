f0c57431efaa1105293273ce0f5772777094f6b5
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-16 11:22:08 -0300

    Update index.php

--- ex003/index.php
+++ ex003/index.php
@@ -6,8 +6,17 @@
     <title>Tipos Primitivos em PHP</title>
 </head>
 <body>
+    <h1>Teste de tipos primitivos </h1>
     <?php 
-        k
+        // $v = 300;
+        // var_dump($v); // código para testar o tipo primitivo
+
+        // $num = 120; 
+        // echo "O valor é $num";
+
+        // $n = 3e2; // 3.10^2;
+        // echo "O valor é $num";
+
     ?>
     
 </body>
