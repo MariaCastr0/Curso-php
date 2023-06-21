d9e6b010916835baca4ab0b37f481c6fa41eaf8c
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-21 09:22:26 -0300

    Update cad.php

--- ex004/cad.php
+++ ex004/cad.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -12,8 +13,11 @@
     </header>
     <main>
         <?php 
-            
+            $n = $_GET["nome"];
+            $s = $_GET["sobrenome"];
+            echo "<p>É um prazer te conhecer, <strong>$n $s</strong>! Este é o meu site!"
         ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
     </main>
     
 </body>
