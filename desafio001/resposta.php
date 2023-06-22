9c5c3807550ce6b5847de5e269a0da44f08f63b3
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-22 09:59:08 -0300

    Update resposta.php

--- desafio001/resposta.php
+++ desafio001/resposta.php
@@ -3,19 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resposta</title>
+    <title>Resposta Desafio</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        $num = 10;
+    <main>
+        <h1>Resultado Final</h1>
+        <p>
+        <?php 
+        $n = $_REQUEST["num"] ?? 0;
+        $a = $n - 1;
+        $s = $n + 1;
       
         //   $ant = $num - 1;
         //   $suc = $num + 1;
     
-          echo "O seu número é $num. \n";
-          echo "O antecessor de $num é ". ($num - 1) .". \n";
-          echo "O sucessor de $num é ". ($num + 1) .". \n";
+          echo "O seu número é $n.;
+          echo "O antecessor de $n é $a.";
+          echo "O sucessor de $n é $s.";
     ?>
-    
+        </p>
+    </main>    
 </body>
 </html>
