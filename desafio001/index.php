4645e1263c90f55f6bb635b8470c613f7265b3a9
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-22 09:13:53 -0300

    Update code

---
+++ desafio001/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+      $num = 15;
+      
+    //   $ant = $num - 1;
+    //   $suc = $num + 1;
+
+      echo "O seu número é $num. \n";
+      echo "O antecessor de $num é ". ($num - 1) .". \n";
+      echo "O sucessor de $num é ". ($num + 1) .". \n";
+    ?>
+</body>
+</html>
