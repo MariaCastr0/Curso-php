5d26c015aaf6d5214be29375b6a0ae5b8be79634
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-23 08:56:23 -0300

    Update

---
+++ desafio002/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 2 - PHP: Sorteador de números aleatórios</title>
+</head>
+<body>
+    <?php 
+        ç
+    ?>
+    
+</body>
+</html>
