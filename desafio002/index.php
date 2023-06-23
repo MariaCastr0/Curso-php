9819abd06efff1acea1cd902cabe0881943ed9d5
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-06-23 09:00:45 -0300

    Update index.php

--- desafio002/index.php
+++ desafio002/index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 2 - PHP: Sorteador de números aleatórios</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        ç
-    ?>
+    <main>
+        <h1>Trabalhando com números aleatórios</h1>
+    </main>
+
     
 </body>
 </html>
